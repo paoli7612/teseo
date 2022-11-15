ca07d5640a6ace45cee7f26ec16c35e54b898a68
paoli7612 <tomaoli7612@gmail.com>
2022-11-16 00:23:58 +0100

    students sample

--- students/models.py
+++ students/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    immagine = models.ImageField()
+    nome = models.CharField(max_length=16)
+    cognome = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.cognome + " " + self.nome
+    
--- students/views.py
+++ students/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 def students(request):
-    return render(request, "students/students.html")
+    return render(request, "students/students.html", {'students': Student.objects.all()})
