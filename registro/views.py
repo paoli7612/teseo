b2b31a8c257d8d2ff9a6d5e6f8ae733c20e416e5
paoli7612 <tomaoli7612@gmail.com>
2022-12-06 13:59:17 +0100

    do login ok

--- registro/views.py
+++ registro/views.py
@@ -25,6 +25,13 @@ def account(request):
         return redirect('login')
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return redirect('home')
     return render(request, 'registro/registration/login.html')
 
 def logout(request):
