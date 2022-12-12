43b945acf842efb701fb74e978940578d949890e
Tommaso Paoli <tomaoli7612@gmail.com>
2022-12-12 02:30:42 +0100

    templates folder

--- teseo/settings.py
+++ teseo/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'teseo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['registro'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
