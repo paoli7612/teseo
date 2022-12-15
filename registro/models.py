24871d8c282d9f4e897c74534d54ba5fe4756fc0
Tommaso Paoli <tomaoli7612@gmail.com>
2022-12-15 21:52:44 +0100

    student and course

--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django_extensions.db.fields import AutoSlugField
 
 # Create your models here.
+
+class Student(models.Model):
+
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32, null=True)
+    birth = models.DateField()
+    slug = AutoSlugField(max_length=32, unique=True, populate_from=('name',))
+
+    def complete_name(self):
+        return self.surname + " " + self.name
+
+    def __str__(self):
+        return self.complete_name()
+    
+class Course(models.Model):
+
+    name = models.CharField(max_length=32)
