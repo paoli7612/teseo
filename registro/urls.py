4d3bf8bdc1883b5ce8afaa4037812e58e5f5bead
Tommaso Paoli <tomaoli7612@gmail.com>
2022-12-15 20:45:29 +0100

    login and logout marco01

--- registro/urls.py
+++ registro/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="nome"),
+    path('', views.home, name="home"),
+    path('login', views.login, name="login"),
+    path('logout', views.logout, name="logout"),
+    path('account', views.account, name="account"),
 ]
--- registro/views.py
+++ registro/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django import urls
 # Create your views here.
 
 def home(request):
     return render(request, 'index.html')
+
+def login(request):
+    if request.method == 'POST':
+        username, password = request.POST['username'], request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return redirect(urls.reverse('account'))
+    return render(request, 'auth/login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect(urls.reverse('home'))
+
+def account(request):
+    return render(request, 'auth/account.html', {
+        'user': request.user
+    })
